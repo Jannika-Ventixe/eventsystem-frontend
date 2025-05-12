c6fb974e25394a6994af38431b9743d274ab3bd9
Jannika Gustafsson <jannikagustafsson@gmail.com>
2025-05-12 13:11:37 +0200

    init

---
+++ src/assets/layouts/PortalLayout.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+import Nav from '../components/Nav'
+
+const PortalLayout = () => {
+  return (
+    <div className="portal-wrapper">
+
+      <Nav />
+      <Header />
+        <main>
+            <Outlet />
+        </main>
+        <Footer />
+    </div>
+  )
+}
+
+export default PortalLayout
